22a023cbf7c095a89f2362ceda19b26b619e01ad
renanll <renan_ll@hotmail.com>
2024-02-08 10:40:55 -0300

    Adicionando os components

---
+++ src/components/ImageDisplay.js
@@ -0,0 +1,3 @@
+// Vai ser responsavel para exibir a imagem da fruta enviada pelo usuario
+// aplicar estilização necessaria
+// Assim utilizaria a tag <img> com atributos src definido para URL da imagem enviada
---
+++ src/components/UploadForm.js
@@ -0,0 +1,6 @@
+// Este componente sera responsavel por fornecer ao usuario o formulario para fazer o upload da imagem
+// Pode incluir campos para selecionar uma imagem do dispositivo do usuario e um botao
+// para enviar o formulario
+// ele tambem pode incluir a logica para manipular o evento de envio do formulario e enviar a imagem
+// para o back end
+
