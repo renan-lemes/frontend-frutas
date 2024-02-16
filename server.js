2c5503f2cfdb18e4fd80f292b13ef22f2df0863b
renanll <renan_ll@hotmail.com>
2024-02-16 19:04:22 -0300

    Chores:arrumei o bug do front

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const path = require('path');
 
 const app = express();
 
-// Define o diretório onde estão os arquivos estáticos (CSS, JavaScript, imagens, etc.)
+require("dotenv").config();
+
+// Define o diretório onde estão os arquivos estáticos (CSS, JavaScript, imagens, etc.) 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Define uma rota para servir o arquivo HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}/`);
