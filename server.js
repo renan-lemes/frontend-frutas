72039228892c73eba3898a861655a72806cc136c
renanll <renan_ll@hotmail.com>
2024-02-20 22:27:33 -0300

    Chores:Arrumeis as todas certas no server

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
+
 const path = require('path');
 
 const app = express();
 
 require("dotenv").config();
-require("./src/bs.js")
+require('./src/bs.js')
 
-// Define o diretório onde estão os arquivos estáticos (CSS, JavaScript, imagens, etc.) 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Define uma rota para servir o arquivo HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
+
+const pictureRouter = require('./src/routes/picture.js');
+
+app.use("/picture", pictureRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}/`);
